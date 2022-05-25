55eda90d61dd1857038093721fbb285a5d88e6ac
Oscar Freij <otg020313@gmail.com>
2022-05-25 22:13:28 +0200

    Add footer
    Added basic footer with quote from employee at
    knifetown burgers in knivsta.

---
+++ private_html/modules/footer.php
@@ -0,0 +1,11 @@
+<footer class="mt-auto">
+    <div class="container-flex">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <span class="navbar-text">
+                    "Gör burgare för att de ska vara goda. Inte för att tjäna pengar." - Fredrik Andersson
+                </span>
+            </div>
+        </nav>
+    </div>
+</footer>
