3f0914591b4f6383bc8dd8b967fa256ad53896fe
Oscar Freij <otg020313@gmail.com>
2022-05-25 22:08:40 +0200

    Add navbar
    Added navbar and nav-link text highlight on active.

---
+++ public_html/static/js/navbar.js
@@ -0,0 +1,18 @@
+function SetActivePage() {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+      });
+      let value = params.page;
+
+      for (let index = 0; index < $(".nav-link").length; index++) {
+        const element = $(".nav-link")[index];
+        if (element.href.includes(value))
+        {
+            element.classList.add("active");
+            element.setAttribute("aria-current", "page");
+            console.log("current page: "+value);
+        }
+      }
+}
+
+SetActivePage();
