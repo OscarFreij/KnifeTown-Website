831e6246ee3fb900596e9558c558f6bf268d452a
Oscar Freij <otg020313@gmail.com>
2022-05-27 15:20:55 +0200

    Add contact page
    Added contact page with contact form.
    Note: Form dose not send email at this time
    as it is only a markup of the final product.

---
+++ private_html/pages/contact.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="row my-3">
+        <span class="fs-1 text-center text">
+            Contact Form
+        </span>
+    </div>
+    <div class="row">
+        <span class="fs-4 text-center text my-3">
+                Write your message and contact information below and we will be in touch as soon as we can :)
+            </span>
+        <form class="container" method="POST">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address (Required)</label>
+                <input type="email" class="form-control" id="email" placeholder="name@domain.com" require>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Phone number</label>
+                <input type="tel" class="form-control" id="exampleFormControlInput1" placeholder="+46 123456789">
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Message (Required)</label>
+                <textarea class="form-control" id="message" rows="5"></textarea>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-success mb-3 col-12">Confirm identity</button>
+            </div>
+    </form>
+    </div>
+</div>
