4576c5e933aeedd8c6e918da99168a3bdf57a320
Oscar Freij <otg020313@gmail.com>
2022-05-27 14:35:52 +0200

    Add find us page
    Added find us page with integrated map from Google.

---
+++ private_html/pages/findus.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row my-3">
+        <span class="fs-1 text-center text">
+            Find Us!
+        </span>
+    </div>
+    <div class="row">
+        <div class="container col-12 col-md-6">
+            <span class="fs-4 text">
+                This is where you find us! 
+            </span>
+            <br>
+            <span class="fs-5 text">
+                Adress: Stationsvägen 2, 741 30 Knivsta
+            </span>
+        </div>
+        <div class="container col-12 col-md-6">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1691.407983902509!2d17.785137976135005!3d59.7255966634627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465fb9b9dd7058a5%3A0x8fb2e38fc016f6ca!2sKnifetown%20Burgers!5e0!3m2!1ssv!2sse!4v1653653397197!5m2!1ssv!2sse" height="450" style="border:0; width: -webkit-fill-available;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+</div>
