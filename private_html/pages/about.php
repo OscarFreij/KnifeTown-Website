b9101d5a891ec903257123a44368512ab8ec5371
Oscar Freij <otg020313@gmail.com>
2022-05-28 19:14:21 +0200

    Add lorem ipsum to about us page
    Added lorem ipsum text to about page to fill
    it out for presentation.

---
+++ private_html/pages/about.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row my-3">
+        <span class="fs-1 text-center text">
+            Who are we and what do we do?
+        </span>
+    </div>
+    <div class="row">
+        <div class="text mb-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo donec enim diam vulputate ut pharetra sit. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Morbi quis commodo odio aenean sed adipiscing diam. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Orci a scelerisque purus semper eget duis at. Viverra justo nec ultrices dui sapien eget mi proin sed. Elit pellentesque habitant morbi tristique senectus et netus et. Non tellus orci ac auctor augue. Viverra maecenas accumsan lacus vel. Odio ut sem nulla pharetra diam sit.
+        </div>
+        <div class="text mb-3">
+        Ut sem viverra aliquet eget sit. Massa tincidunt nunc pulvinar sapien et ligula. Nec sagittis aliquam malesuada bibendum arcu vitae elementum. Leo duis ut diam quam nulla porttitor massa. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Amet est placerat in egestas erat imperdiet sed. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Pellentesque id nibh tortor id aliquet lectus proin. Vitae congue eu consequat ac felis donec et. Nunc consequat interdum varius sit amet mattis vulputate enim. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Morbi non arcu risus quis varius quam quisque. Netus et malesuada fames ac turpis egestas integer eget aliquet. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Ultricies integer quis auctor elit sed. Sed enim ut sem viverra aliquet eget sit amet. In hac habitasse platea dictumst vestibulum rhoncus est.
+        </div>
+    </div>
+</div>
