9ef8769e64106bae99326714f5b0f600336ef3d5
Oscar Freij <otg020313@gmail.com>
2022-05-28 18:46:52 +0200

    Fix footer text pos on mobile
    Fixed text position in the footer for mobile
    units.

--- private_html/modules/footer.php
+++ private_html/modules/footer.php
@@ -2,16 +2,16 @@
     <div class="container-flex">
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
-                <span class="navbar-text col-4 text-start">
+                <span class="navbar-text col-12 col-md-4 text-md-start text-center">
                     "Vi gör burgare för att de ska vara goda.<br>Inte för att tjäna pengar." - Fredrik Andersson
                 </span>
-                <span class="col-4 text-center">
+                <span class="col-12 col-md-4 text-md-center text-center">
                     <div>
                         <a onclick="setLanguage('se');"><img src="static/media/images/swe_flag_1.png" alt="" height="24px" width="32px"></a>
                         <a onclick="setLanguage('en');"><img src="static/media/images/us-brit_flag_1.png" alt="" height="24px" width="32px"></a>
                     </div>
                 </span>
-                <span class="navbar-text col-4 text-end">
+                <span class="navbar-text col-12 col-md-4 text-md-end text-center">
                     Knifetown Burgers &COPY; 2022
                 </span>
             </div>
