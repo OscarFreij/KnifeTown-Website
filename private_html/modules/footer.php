8bb3f9b51f6c562c97fa9ac106f7fa8012476011
Oscar Freij <otg020313@gmail.com>
2022-06-14 21:32:03 +0200

    Remove translation settings
    Removed translation settings as most browsers already
    have built in translators.
    
    ToDo: convert page to swedish.

--- private_html/modules/footer.php
+++ private_html/modules/footer.php
@@ -7,12 +7,6 @@
                     $container->functions()->getCustomPageContent('quoteFooter');
                     ?>
                 </span>
-                <span class="col-12 col-md-4 text-md-center text-center">
-                    <div>
-                        <a onclick="setLanguage('se');"><img src="static/media/images/swe_flag_1.png" alt="" height="24px" width="32px"></a>
-                        <a onclick="setLanguage('en');"><img src="static/media/images/us-brit_flag_1.png" alt="" height="24px" width="32px"></a>
-                    </div>
-                </span>
                 <span class="navbar-text col-12 col-md-4 text-md-end text-center">
                     Knifetown Burgers &COPY; 2022
                 </span>
--- public_html/static/js/main.js
+++
@@ -1,7 +0,0 @@
-function setLanguage(languageCode) {
-    document.cookie = "language="+languageCode;
-}
-
-function getLanguage() {
-    console.log("current cookies: "+document.cookie);
-}
