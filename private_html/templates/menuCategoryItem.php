7b0f27c7ee705fbe8c6cb992199a37a6ab8fb216
Oscar Freij <otg020313@gmail.com>
2022-06-14 23:24:45 +0200

    Add blob loading from DB
    Added loading of menuItems images to load
    from blobs in DB.

--- private_html/templates/menuCategoryItem.php
+++ private_html/templates/menuCategoryItem.php
@@ -1,10 +1,10 @@
 <div class="col">
     <div class="card h-100">
         <?php
-            if (is_null($item['imageURL']))
+            if (is_null($item['imageData']))
             {
                 ?>
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <svg class="bd-placeholder-img card-img-top menuCategoryItem-picture" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <title>Placeholder</title>
                     <rect width="100%" height="100%" fill="#868e96"></rect>
                     <text x="50%" y="50%" fill="#dee2e6" dy=".3em" dominant-baseline="middle" text-anchor="middle">
@@ -16,7 +16,7 @@
             else
             {
                 ?>
-                <img class="card-img-top" src="<?=$item['imageURL']?>" alt="pictue of product" width="100%" height="140">
+                <img class="card-img-top menuCategoryItem-picture" src="<?=$item['imageData']?>" alt="pictue of product">
                 <?php
             }
         ?>
