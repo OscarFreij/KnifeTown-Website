d700202e3db201510323e33c6b2dffe1290ab4b4
Oscar Freij <otg020313@gmail.com>
2022-06-27 23:05:29 +0200

    Add login and user functions
    Added login and user functions as prep
    for editor.

--- private_html/classes/functions.class.php
+++ private_html/classes/functions.class.php
@@ -392,5 +392,149 @@ class functions
     }
 
     #endregion
+
+    #region User Functions
+    private function checkLogin(string $username, string $password)
+    {
+        $quotedUsername = $this->container->db()->quote($username);
+        $result = $this->container->db()->constructResultQuerry("SELECT `password`, `enabled` FROM `users` WHERE `username` = $quotedUsername");
+
+        if ($this->checkUserExists($username))
+        {
+            if ($result[0]['enabled'] == 1)
+            {
+                $hash = $result[0]['password'];
+                return password_verify($password, $hash);
+            }
+            else
+            {
+                error_log("User ".$quotedUsername." tried to login but is disabled");
+            }
+        }
+        else
+        {
+            error_log("Unknown user tried to login: ".$quotedUsername);
+        }
+        return false;
+    }
+
+    public function changePassword(string $username, string $passwordOld, string $passwordNew)
+    {
+        if ($this->checkLogin($username, $passwordOld))
+        {
+            $quotedUsername = $this->container->db()->quote($username);
+            $quotedPasswordNewHASH = $this->container->db()->quote(password_hash($passwordNew, PASSWORD_DEFAULT));
+            $success = $this->container->db()->constructQuerry("UPDATE `users` SET `password`=$quotedPasswordNewHASH WHERE `username` = $quotedUsername AND `enabled` = 1;");
+        }
+        else
+        {
+            $success = false;
+        }
+        
+        return $success;
+    }
+
+    public function createAccount(string $username, string $password)
+    {
+        if (!$this->checkUserExists($username))
+        {
+            $quotedUsername = $this->container->db()->quote($username);
+            $quotedPasswordHASH = $this->container->db()->quote(password_hash($password, PASSWORD_DEFAULT));
+            $success = $this->container->db()->constructQuerry("INSERT INTO `users` (`username`, `password`) VALUES ($quotedUsername, $quotedPasswordHASH)");
+        }
+        else
+        {
+            $success = false;
+            error_log("Can not create user. User exists!");
+        }
+        
+        return $success;
+    }
+
+    public function deleteAccount(string $username)
+    {
+        if ($this->checkUserExists($username))
+        {
+            $quotedUsername = $this->container->db()->quote($username);
+            $success = $this->container->db()->constructQuerry("DELETE FROM `users` WHERE `username` = $quotedUsername");
+        }
+        else
+        {
+            $success = false;
+        }
+        
+        return $success;
+    }
+
+    private function getUserData(string $username)
+    {
+        $quotedUsername = $this->container->db()->quote($username);
+        return $this->container->db()->constructResultQuerry("SELECT `id`, `superAdmin`, `enabled` FROM `users` WHERE username = $quotedUsername;");
+    }
+
+    private function checkUserExists(string $username)
+    {
+        $quotedUsername = $this->container->db()->quote($username);
+        if (count($this->container->db()->constructResultQuerry("SELECT `id`, `enabled` FROM `users` WHERE username = $quotedUsername;")) != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public function toggleUserEnabled(string $username)
+    {
+        $quotedUsername = $this->container->db()->quote($username);
+        $result = $this->getUserData($username);
+
+        if ($result[0]['enabled'] == 0)
+        {
+            $success = $this->container->db()->constructQuerry("UPDATE `users` SET `enabled` = 1 WHERE `username` = $quotedUsername;");
+        }
+        else
+        {
+            $success = $this->container->db()->constructQuerry("UPDATE `users` SET `enabled` = 0 WHERE `username` = $quotedUsername;");
+        }
+        return $success;
+    }
+
+    public function login(string $username, string $password)
+    {
+        if ($this->checkLogin($username, $password))
+        {
+            $result = $this->getUserData($username);
+
+            if (session_status() != PHP_SESSION_ACTIVE) {
+                session_start();
+            }
+            $_SESSION['id'] = $result[0]['id'];
+            $_SESSION['username'] = $username;
+            $_SESSION['superAdmin'] = $result[0]['superAdmin'];
+            error_log("UserLogin Successfull: ".$username);
+            return true;
+        }
+        else
+        {
+            return false;
+            error_log("UserLogin Failed: ".$username);
+        }
+    }
+
+    public function logout()
+    {
+        if (session_status() != PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+        session_destroy();
+
+        $host  = $_SERVER['HTTP_HOST'];
+        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        header("Location: http://$host$uri/", true);
+    }
+    #endregion
+
 }
 ?>
