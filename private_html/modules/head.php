7952235c9d56a679091a8e8b974b7d2e9260c4dd
Oscar Freij <otg020313@gmail.com>
2022-06-29 15:09:04 +0200

    Fix double navbar not sticking
    Fixed double navbar for editor not sticking to
    "top" when scorlled.

--- private_html/modules/head.php
+++ private_html/modules/head.php
@@ -7,4 +7,32 @@
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="static/css/master.css">
+
+    <?php
+    if (isset($_GET['page']))
+    {
+        if (file_exists("static/css/".$_GET['page'].".css"))
+        {
+            ?>
+        <link rel="stylesheet" href="static/css/<?=$_GET['page']?>.css">
+        <?php
+        }
+
+        if ($_GET['page'] == "edit")
+        {
+            if(isset($_GET['editorPage']))
+            {
+                if (file_exists("static/css/edit".$_GET['editorPage'].".css"))
+                {
+                    ?>
+                <link rel="stylesheet" href="static/css/edit<?=$_GET['editorPage']?>.css">
+                <?php
+                }
+            }
+        }
+        
+    }
+    
+    ?>
+
 </head>
